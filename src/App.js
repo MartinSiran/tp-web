bdf7c8d0430e4a81556cc65a11ea3f4f5055c931
MartinSiran <matosiro4@gmail.com>
2022-09-23 16:54:47 +0200

    add offer page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes, Link } from "react-router-dom";
 import './App.css';
 import Assignment from "./components/Assignment";
 import Home from "./components/Home";
+import Offer from "./components/Offer"
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/assignment" element={<Assignment />} />
+        <Route path="/offer" element={<Offer />} />
       </Routes>
     </div>
   );
---
+++ src/components/Offer.js
@@ -0,0 +1,13 @@
+import Alert from 'react-bootstrap/Alert';
+
+const Assignment = () => {
+  return (
+    <div className="App">
+      <Alert variant='warning'>
+        Offer
+      </Alert>
+    </div>
+  );
+}
+
+export default Assignment;
