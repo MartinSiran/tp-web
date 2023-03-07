b238bf8d5c2b491fd9d074ab5e1117fd1363737d
MartinSiran <matosiro4@gmail.com>
2023-03-07 14:02:55 +0100

    update reports component, add test reports

--- src/components/Reports.js
+++ src/components/Reports.js
@@ -11,6 +11,7 @@ const Reports = () => {
   };
 
   const [files, setFiles] = useState(importAll(require.context("../reports", false, /\.pdf$/)))
+  const [files2, setFiles2] = useState(importAll(require.context("../reports2", false, /\.pdf$/)))
   console.log(files)
 
   files.forEach(file => {
@@ -20,9 +21,16 @@ const Reports = () => {
   return (
     <Container>
       <h1 class="mt-3">Zápisnice</h1>
+      <h2 className="mt-2">Zimný semester</h2>
         {files.map((file, index) => (
           <Row>
-            <Report key={file.toString()} file={file} fileName={"Zápisnica - " + (index + 1) + ". týždeň"} />
+            <Report key={file.toString()} file={file} fileName={"Zápisnica č. " + (index + 1)} />
+          </Row>
+        ))}
+      <h2 className="mt-2">Letný semester</h2>
+        {files2.map((file, index) => (
+          <Row>
+            <Report key={file.toString()} file={file} fileName={"Zápisnica č. " + (index + 1)} />
           </Row>
         ))}
     </Container>
