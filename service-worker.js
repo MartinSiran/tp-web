04ffbc00edb4f3d484aa5e9b0afd46027275a8d9
MartinSiran <matosiro4@gmail.com>
2023-04-04 15:22:00 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tp-web/precache-manifest.67dd145f3b21cd1305207c62874287cf.js"
+  "/tp-web/precache-manifest.cd232a4223c05f5e7a364eb1bdb178c9.js"
 );
 
 workbox.clientsClaim();
